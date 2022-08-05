245604ac69e6d9a5b12645ab64ea0a662140f91d
Dean M Greer <38226388+Gcenx@users.noreply.github.com>
2022-08-05 10:28:18 -0400

    Remove “wine” function
    As there’s no real desire to directly call “wine” these lines won’t be required

--- install.sh
+++ install.sh
@@ -2,15 +2,8 @@
 
 WINESKIN_TARGET_NAME="Sims4.app"
 
-export WINEDEBUG="-all"
-export WINEDLLOVERRIDES="mscoree="
-export WINETRICKS_FALLBACK_LIBRARY_PATH="${PWD}/${WINESKIN_TARGET_NAME}/Contents/Frameworks"
-export WINEPREFIX="${PWD}/${WINESKIN_TARGET_NAME}/Contents/SharedSupport/prefix"
-export wineWrappers="${PWD}/${WINESKIN_TARGET_NAME}/Wineskin.app/Contents/Resources"
-
 wineskinlauncher(){ ${PWD}/${WINESKIN_TARGET_NAME}/Contents/MacOS/wineskinlauncher "${@}";}
 winetricks(){ wineskinlauncher WSS-winetricks "${@}";}
-wine(){ ${wineWrappers}/wine "${@}" >/dev/null 2>&1;}
 
 echo "==> Removing Gatekeeper quarantine from downloaded wrapper. You may need to enter your password."
 sudo xattr -drs com.apple.quarantine "${PWD}/${WINESKIN_TARGET_NAME}" &>/dev/null
