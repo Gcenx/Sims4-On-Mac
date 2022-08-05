e0d733f84fa39c69b67cdede23d5355d475aafb3
Dean M Greer <gcenx83@gmail.com>
2022-08-05 10:05:37 -0400

    More fixes
    - Wrap wineskinlauncher for winetricks
    - Remove now unneeded 7z binary
    - Fix Sims4 Icon
    - Some additional tweaks

--- install.sh
+++ install.sh
@@ -2,22 +2,18 @@
 
 WINESKIN_TARGET_NAME="Sims4.app"
 
-export wineWrappers="${PWD}/${WINESKIN_TARGET_NAME}/Wineskin.app/Contents/Resources"
-export PATH="${wineWrappers}:${PATH}"
 export WINEDEBUG="-all"
-
+export WINEDLLOVERRIDES="mscoree="
 export WINETRICKS_FALLBACK_LIBRARY_PATH="${PWD}/${WINESKIN_TARGET_NAME}/Contents/Frameworks"
 export WINEPREFIX="${PWD}/${WINESKIN_TARGET_NAME}/Contents/SharedSupport/prefix"
+export wineWrappers="${PWD}/${WINESKIN_TARGET_NAME}/Wineskin.app/Contents/Resources"
+
+wineskinlauncher(){ ${PWD}/${WINESKIN_TARGET_NAME}/Contents/MacOS/wineskinlauncher "${@}";}
+winetricks(){ wineskinlauncher WSS-winetricks "${@}";}
+wine(){ ${wineWrappers}/wine "${@}" >/dev/null 2>&1;}
 
 echo "==> Removing Gatekeeper quarantine from downloaded wrapper. You may need to enter your password."
 sudo xattr -drs com.apple.quarantine "${PWD}/${WINESKIN_TARGET_NAME}" &>/dev/null
-
-function install_deps() {
-    echo "===> Installing dotnet48"
-    winetricks -q -f dotnet48 &>/dev/null
-    echo "===> Installing Origin"
-    winetricks -q -f origin
-}
 
 echo "==> Verifying winetricks is installed within wrapper."
 ${PWD}/${WINESKIN_TARGET_NAME}/Wineskin.app/Contents/Resources/winetricks list-installed &>/dev/null
@@ -29,9 +25,13 @@ if [ "$isWorkingEnv" != "0" ]; then
     chmod +x ${PWD}/${WINESKIN_TARGET_NAME}/Wineskin.app/Contents/Resources/winetricks &>/dev/null
 fi
 
-echo "==> Installing proprietary dependencies and Origin..."
-install_deps
-echo "==> Finished installing dependencies."
+
+
+
+echo "===> Setting Windows 7"
+winetricks win7
+echo "===> Installing Origin"
+winetricks -q -f origin
 
 echo "==> Moving Sims4.app to your Applications folder"
 sudo rsync -l -a -r ${PWD}/${WINESKIN_TARGET_NAME}/ /Applications/Sims4.app
