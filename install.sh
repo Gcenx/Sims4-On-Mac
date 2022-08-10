b11b2908da4b7ba7afbdad0d2a3b78426fbe7dec
Dean M Greer <gcenx83@gmail.com>
2022-08-09 22:11:59 -0400

    overhaul the script
    This will still require a release archive, but at least now the archive will be negligible

--- install.sh
+++ install.sh
@@ -1,8 +1,28 @@
 #!/usr/bin/env arch -x86_64 bash
 
 TARGET_NAME="Sims4.app"
+WRAPPER_VERSION=2.9.1.1
+ENGINE_VERSION=21.2.0
 
-sudo rsync -l -a -r ${PWD}/TEMPLATE.app/ ${PWD}/${TARGET_NAME}/
+clear
+
+echo "#########################################################################"
+echo "################################ ${TARGET_NAME} ##############################"
+echo "#########################################################################"
+echo ""
+echo "==> Downloading Wrapper Template"
+curl -LJO --progress-bar https://github.com/The-Wineskin-Project/Wrapper/releases/download/${WRAPPER_VERSION}/Wineskin-${WRAPPER_VERSION}.app.tar.xz
+mkdir -p ${PWD}/${TARGET_NAME}
+tar -xf Wineskin-${WRAPPER_VERSION}.app.tar.xz --strip-components=1 -C ${PWD}/${TARGET_NAME}; rm ${PWD}/Wineskin-${WRAPPER_VERSION}.app.tar.xz
+echo ""
+echo "==> Downloading Engine"
+curl -LJO --progress-bar https://github.com/The-Wineskin-Project/Engines/releases/download/v1.0/WS11WineCX64Bit${ENGINE_VERSION}.tar.xz
+mkdir -p ${PWD}/${TARGET_NAME}/Contents/SharedSupport/wine
+tar -xf WS11WineCX64Bit${ENGINE_VERSION}.tar.xz --strip-components=1 -C ${PWD}/${TARGET_NAME}/Contents/SharedSupport/wine; rm ${PWD}/WS11WineCX64Bit${ENGINE_VERSION}.tar.xz
+
+# Change Winesin.icns to Sims4 logo
+cp -f ${PWD}/configs/Sims4.icns ${PWD}/${TARGET_NAME}/Contents/Resources/Wineskin.icns
+touch ${PWD}/${TARGET_NAME}
 
 export WINEPREFIX="${PWD}/${TARGET_NAME}/Contents/SharedSupport/prefix"
 wineskinlauncher(){ ${PWD}/${TARGET_NAME}/Contents/MacOS/wineskinlauncher "${@}";}
@@ -10,24 +30,22 @@ wineboot(){ wineskinlauncher WSS-wineprefixcreate >/dev/null 2>&1;}
 winetricks(){ wineskinlauncher WSS-winetricks "${@}";}
 override_dll(){ ${PWD}/${TARGET_NAME}/Wineskin.app/Contents/Resources/wine reg add 'HKEY_CURRENT_USER\Software\Wine\DllOverrides' /v "${@}" /d native,builtin /f >/dev/null 2>&1;}
 
-echo "==> Removing Gatekeeper quarantine from downloaded wrapper. You may need to enter your password."
-sudo xattr -drs com.apple.quarantine "${PWD}/${TARGET_NAME}" &>/dev/null
-
+echo ""
 echo "==> Downloading winetricks."
-curl -o ${PWD}/${TARGET_NAME}/Wineskin.app/Contents/Resources/winetricks https://raw.githubusercontent.com/The-Wineskin-Project/winetricks/macOS/src/winetricks &>/dev/null
+curl --progress-bar -o ${PWD}/${TARGET_NAME}/Wineskin.app/Contents/Resources/winetricks https://raw.githubusercontent.com/The-Wineskin-Project/winetricks/macOS/src/winetricks
 chmod +x ${PWD}/${TARGET_NAME}/Wineskin.app/Contents/Resources/winetricks &>/dev/null
 
 
 
 
-# Failsafe as git doesn't remove even empty directories
-rm -rf ${WINEPREFIX} &>/dev/null
-
+echo ""
 echo "===> Creating wineprefix"
 wineboot
 
+echo ""
+echo "===> Do you own Sims4 through Steam or Origin?"
 while true; do
-  read -p "===> Do you own Sims4 through Steam or Origin? Please Type the Origin or Steam and hit Enter: " -r platform
+  read -p "Please Type the Origin or Steam and hit Enter: " -r platform
   case $platform in
     Origin | origin ) platform=origin; break;;
     Steam | steam ) platform=steam; break;;
@@ -35,6 +53,7 @@ while true; do
   esac
 done
 
+echo ""
 if [ $platform == origin ]; then
   echo "===> Installing Origin, This may take a while."
   winetricks -q -f origin
@@ -45,10 +64,11 @@ elif [ $platform == steam ]; then
   cp -f ${PWD}/configs/Steam.plist ${PWD}/${TARGET_NAME}/Contents/Info.plist
 fi
 
-
-
+echo ""
 echo "===> Moving Sims4.app to your Applications folder"
-sudo rsync -l -a -r ${PWD}/${TARGET_NAME}/ /Applications/${TARGET_NAME}
+rsync -l -a -r ${PWD}/${TARGET_NAME}/ /Applications/${TARGET_NAME}
 rm -rf ${TARGET_NAME}
+
+echo ""
 echo "===> launching Sims4.app"
 open -a /Applications/Sims4.app
